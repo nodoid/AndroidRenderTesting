69c56e5210db3455a3cd48be4411894053069f66
Paul Johnson <paul@snuberry-software.co.uk>
2017-12-29 11:03:05 +0000

    Initial import

---
+++ AndroidRenderTesting/Activity1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+using System.IO;
+
+namespace AndroidRenderTesting
+{
+    [Activity(Label = "AndroidRenderTesting", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            WebView webview = FindViewById<WebView>(Resource.Id.webView1);
+            webview.Settings.JavaScriptEnabled = true;
+
+            StreamReader reader = new StreamReader(Assets.Open("RenderHTML.html"));
+            webview.LoadDataWithBaseURL(null, reader.ReadToEnd(), "text/html", "UTF-8", null);
+
+            reader.Close();
+        }
+    }
+}
+
